e8a9eaceb3b9bbf328eff0d321be7fc770fa8553
Jason Hsu <junchoon14@gmail.com>
2018-08-12 16:10:34 +0800

    8.12/2018 ver1

--- GameOfThrone/WebViewController.swift
+++ GameOfThrone/WebViewController.swift
@@ -15,10 +15,10 @@ class WebViewController: UIViewController {
     @IBOutlet weak var webView: WKWebView!
     
     @IBAction func goBack(_ sender: Any) {
-        webView.goBack()
+        webView!.goBack()
     }
     @IBAction func goForward(_ sender: Any) {
-        webView.goForward()
+        webView!.goForward()
     }
     
 
@@ -27,7 +27,7 @@ class WebViewController: UIViewController {
 
        let url = URL(string: "http://zh.asoiaf.wikia.com/wiki/%E5%86%B0%E4%B8%8E%E7%81%AB%E4%B9%8B%E6%AD%8C%E4%B8%AD%E6%96%87%E7%BB%B4%E5%9F%BA")
         let request = URLRequest(url: url!)
-        webView.load(request)
+        webView!.load(request)
        
     }
     
